05767ba8a7cd25a02af9e3137484412711a37557
Oleh <oleggulevic8@gmail.com>
2025-02-12 22:35:55 +0100

    wrap app to react-router-dom

--- airbnb-client/src/App.tsx
+++ airbnb-client/src/App.tsx
@@ -1,31 +1,18 @@
 
 
 import './App.css'
-import Layout from "./Layout.tsx";
-import Home from "./Components/Home/Home.tsx";
 
-import ProperticeByArea from "./Components/ProperticeByAreaComponent/ProperticeByArea.tsx";
-import PropertiecByClassification from "./Components/ProperticeByClassification/PropertiecByClassification.tsx";
-import {FilterProvider} from "./context/FilterContext.tsx";
-import LatestPropertyComponent from "./Components/LatestProprties/LatestPropertyComponent.tsx";
-import ContactUsComponent from "./Components/ContactUs/ContactUsComponent.tsx";
-import Footer from "./Components/Footer/Footer.tsx";
+import {Route, Routes} from "react-router-dom";
+import MainComponent from "./MainComponent.tsx";
 
 
 function App() {
  
 
   return (
-      <FilterProvider>
-    <Layout>
-      <Home/>
-        <ProperticeByArea/>
-        <PropertiecByClassification/>
-      <LatestPropertyComponent/>
-      <ContactUsComponent/>
-      <Footer/>
-    </Layout>
-      </FilterProvider>
+     <Routes>
+       <Route path={"/"} element={<MainComponent/>}/>
+     </Routes>
   )
 }
 
---
+++ airbnb-client/src/MainComponent.tsx
@@ -0,0 +1,32 @@
+﻿
+
+import './App.css'
+import Layout from "./Layout.tsx";
+import Home from "./Components/Home/Home.tsx";
+
+import ProperticeByArea from "./Components/ProperticeByAreaComponent/ProperticeByArea.tsx";
+import PropertiecByClassification from "./Components/ProperticeByClassification/PropertiecByClassification.tsx";
+import {FilterProvider} from "./context/FilterContext.tsx";
+import LatestPropertyComponent from "./Components/LatestProprties/LatestPropertyComponent.tsx";
+import ContactUsComponent from "./Components/ContactUs/ContactUsComponent.tsx";
+import Footer from "./Components/Footer/Footer.tsx";
+
+
+function App() {
+
+
+    return (
+        <FilterProvider>
+            <Layout>
+                <Home/>
+                <ProperticeByArea/>
+                <PropertiecByClassification/>
+                <LatestPropertyComponent/>
+                <ContactUsComponent/>
+                <Footer/>
+            </Layout>
+        </FilterProvider>
+    )
+}
+
+export default App
