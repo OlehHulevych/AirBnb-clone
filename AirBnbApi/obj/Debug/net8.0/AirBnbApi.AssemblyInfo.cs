de021b32239cf47bdb6ad0d0abac97b64986b1b3
Oleh <oleggulevic8@gmail.com>
2025-03-03 20:59:05 +0100

    made and adaptate property page

--- AirBnbApi/obj/Debug/net8.0/AirBnbApi.AssemblyInfo.cs
+++ AirBnbApi/obj/Debug/net8.0/AirBnbApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirBnbApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05767ba8a7cd25a02af9e3137484412711a37557")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e95a635dd87e616bae429cefb0d71e2acc71f8f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirBnbApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirBnbApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- airbnb-client/src/Components/PropertyItem/PropertyItem.tsx
+++ airbnb-client/src/Components/PropertyItem/PropertyItem.tsx
@@ -9,14 +9,63 @@ import { MdOutlinePets } from "react-icons/md";
 import { MdBedroomParent } from "react-icons/md";
 import { TbToolsKitchen3 } from "react-icons/tb";
 import Footer from "../Footer/Footer.tsx";
+import {useState} from "react";
+import { IoCloseSharp } from "react-icons/io5";
+
 
 
 
 const PropertyItem = () => {
+    
+    const stockImages = [
+        {
+            id:0,
+            url:"./stocks/big_image.jpg",
+        },
+        {
+            id:1,
+            url:"./stocks/small_image1.jpg",
+        },
+        {
+            id:2,
+            url:"./stocks/small_image2.jpg"
+        },
+        {
+            id:3,
+            url:"./stocks/big_image.jpg",
+        },
+        {
+            id:4,
+            url:"./stocks/small_image1.jpg",
+        },
+        {
+            id:5,
+            url:"./stocks/small_image2.jpg"
+        }
+    ]
+    
+    const [index, setIndex] = useState<number>(0)
+    const [open, setOpen] = useState<boolean>(false);
+    
+    
+    
+    
+    const nextPhoto =()=>{
+        setIndex(index=>{
+           if(index===stockImages.length+1) return 0
+            return index+1
+        })
+    };
+    const previousPhoto =()=>{
+        setIndex(index=>{
+            if(index===0) return stockImages.length-1
+            return index-1;
+        })
+    };
     return (
         <Layout>
-            <div className={"bg-slate-500 w-full min-h-screen flex flex-col pt-26 relative   "}>
-                <div className={"w-full flex"}>
+            <div className={"bg-slate-500 w-full min-h-screen flex flex-col pt-26 relative     "}>
+                <div className={"w-full flex "}>
                     <div className={`w-1/2 h-auto cursor-pointer after:w-full after:h-full relative after:content-[""] hover:after:bg-black after:absolute after:opacity-40 after:top-0 after:transition-all after:duration-300 after:ease-in-out `}>
                         <img className={"h-full w-full "} src="./stocks/big_image.jpg" alt=""/>
                     </div>
@@ -115,11 +164,24 @@ const PropertyItem = () => {
                         </div>
                         
                     </div>
-                    <div className={"w-full"}>
-                    </div>
+                    
                     <Footer/>
+                    
                 </div>
+                {open &&  <div className={"w-full bg-gray-950/75 fixed min-h-screen top-0 z-200   "}>
+
+                    <div onClick={()=>nextPhoto()} className={ "block w-[30px] h-[30px] border-solid border-r-white border-white border-r-3 border-b-3 top-[50%] left-[2%] absolute transform rotate-135 cursor-pointer hover:scale-115 hover:transition-all z-100"}></div>
+                    <div onClick={()=>previousPhoto()} className={ "block w-[30px] h-[30px] border-solid border-r-white border-white border-r-3 border-b-3 top-[50%] right-[2%] absolute transform rotate-315 cursor-pointer hover:scale-115 z-100 hover:transition-all"}></div>
+                    <div className={`w-full min-h-screen flex justify-between items-center   duration-400 ease-in-out relative  `}>
+                        <IoCloseSharp className={"text-white text-6xl absolute top-2 right-5 cursor-pointer max-[600px]:text-4xl max-[600px]:top-10"}/>
+                        <img className={` w-[70%]  h-[80%] absolute left-[15%] max-[850px]:h-[50%] max-[600px]:h-[40%] max-[600px]:w-[80%] max-[600px]:left-[10%] max-[400px]:h-[30%]   `} src={"./stocks/big_image.jpg"} alt=""/>
+                    </div>
+                </div>  }
+                
+
+                
             </div>
+            
         </Layout>
     );
 };
